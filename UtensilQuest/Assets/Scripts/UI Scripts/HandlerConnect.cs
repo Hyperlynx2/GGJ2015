6a9741523c31dc2e7ac602d61eb9a2f292a29610
Matt Dodd <doddzy39@gmail.com>
2015-01-24 00:43:31 +1100

    Added basic lobby functionality.  Still need to add some helper script and get the Agent lobby working correctly.

---
+++ UtensilQuest/Assets/Scripts/UI Scripts/HandlerConnect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HandlerConnect : MonoBehaviour {
+    public Text IPBox;
+
+    public Text ConnectedNotification;
+    public Text ErrorNotifaction;
+
+    public Button ConnectButton;
+
+	// Use this for initialization
+	public void OnConnectButtonClicked()
+    {
+        NetworkConnectionError err = Network.Connect(IPBox.text, 16048, "Dadnewt");
+        if (err == NetworkConnectionError.NoError)
+        {
+            ConnectedNotification.enabled = true;
+            ErrorNotifaction.enabled = false;
+
+            ConnectButton.enabled = false;
+        }
+        else
+        {
+            ConnectedNotification.enabled = false;
+            ErrorNotifaction.enabled = true;
+        }
+    }
+
+    [RPC]
+    void RPC_OnServerStartGame(string someInfo)
+    {
+        Application.LoadLevel("4 - MainGame");
+    }
+}

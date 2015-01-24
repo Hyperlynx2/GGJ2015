e73b5dfa0050cadc48414bbeb014ac07b6e4ffde
Matt Dodd <doddzy39@gmail.com>
2015-01-24 12:39:49 +1100

    Modified Level parser to handle enemy paths

--- UtensilQuest/Assets/Editor/ParseLevelObjects.cs
+++ UtensilQuest/Assets/Editor/ParseLevelObjects.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ParseLevelObjects : MonoBehaviour
 {
+    static List<List<Transform>> enemyPathList = null;
+
 
     [MenuItem("Game/Parse Level Objects (Reset all values)")]
     static void DoFullLevelParse()
@@ -19,9 +22,19 @@ public class ParseLevelObjects : MonoBehaviour
 
     static void DoLevelParsing(bool bKeepValues)
     {
+        //Reset the state of the world for things that we will be regenerating
         GameManager manager = GameManager.FindObjectOfType<GameManager>();
         manager.secCams.Clear();
 
+        //Destroy all enemies in the level
+        Enemy[] enemyList = GameObject.FindObjectsOfType<Enemy>();
+        foreach (Enemy obj in enemyList)
+        {
+            GameObject.DestroyImmediate(obj.gameObject);
+        }
+
+        //Empty the old path list
+        enemyPathList = new List<List<Transform>>();
 
         GameObject[] goList = GameObject.FindObjectsOfType<GameObject>();
         foreach (GameObject obj in goList)
@@ -40,7 +53,58 @@ public class ParseLevelObjects : MonoBehaviour
             {
                 HandlePlayerSpawnPoint(obj);
             }
+            if(obj.name.Contains("PATHNODE"))
+            {
+                HandlePathNode(obj);
+            }
         }
+
+        CreateEnemiesForPaths();
+    }
+
+    private static void CreateEnemiesForPaths()
+    {
+        foreach(List<Transform> enemyPath in enemyPathList)
+        {
+
+            Object enemyPrefab = Resources.LoadAssetAtPath<GameObject>("Assets/Prefabs/Enemy.prefab");
+            GameObject enemyObj = PrefabUtility.InstantiatePrefab(enemyPrefab) as GameObject;
+
+            Enemy scr = enemyObj.GetComponent<Enemy>();
+            scr.waypoints = enemyPath.ToArray();
+
+
+            enemyObj.transform.position = enemyPath[0].position;
+        }
+    }
+
+    private static void HandlePathNode(GameObject obj)
+    {
+        //Parse the path node for information
+        string pathID = obj.name.Substring("PATHNODE-".Length, 3);
+        string nodeValue = obj.name.Substring("PATHNODE-xxx-".Length, 3);
+
+        int iPathID = int.Parse(pathID);
+        int iNodeValue = int.Parse(nodeValue);
+
+
+        while(iPathID > enemyPathList.Count)
+        {
+            enemyPathList.Add(null);
+        }
+
+        if(enemyPathList[iPathID - 1] == null)
+        {
+            enemyPathList[iPathID - 1] = new List<Transform>();
+        }
+
+        
+        while(iNodeValue > enemyPathList[iPathID - 1].Count)
+        {
+            enemyPathList[iPathID - 1].Add(null);
+        }
+
+        enemyPathList[iPathID - 1][iNodeValue - 1] = obj.transform;
     }
 
     private static void HandlePlayerSpawnPoint(GameObject obj)

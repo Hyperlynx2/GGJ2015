53c4812e00de782a667374be17fa028e01ac436f
Edward <e.whitehead@sae.edu>
2015-01-24 11:44:21 +1100

    Agent behaviour and item collisions. Inventory is working.

--- UtensilQuest/Assets/Scripts/Item.cs
+++ UtensilQuest/Assets/Scripts/Item.cs
@@ -15,4 +15,17 @@ public class Item : MonoBehaviour
 	{
 	
 	}
+
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.name == "Player(Clone)")
+		{
+			int currentInv = hit.GetComponentInChildren<AgentBehaviour>().currentSlot;
+			hit.GetComponentInChildren<AgentBehaviour>().inventory[currentInv] = this.gameObject;
+			hit.GetComponentInChildren<AgentBehaviour>().invIcons[currentInv] = icon;
+			hit.GetComponentInChildren<AgentBehaviour>().currentSlot += 1;
+			this.renderer.enabled = false;
+			this.collider.enabled = false;
+		}
+	}
 }

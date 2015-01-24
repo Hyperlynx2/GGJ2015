f31ef182c3ee8a8cb005408d40602ab5668310d7
Edward <e.whitehead@sae.edu>
2015-01-24 12:39:52 +1100

    Added the door and scaled up the prefabs.

---
+++ UtensilQuest/Assets/Scripts/Door.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour 
+{
+	public bool isLocked;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			Locking (!isLocked);
+		}
+	}
+
+	void Locking(bool lockStatus)
+	{
+		if(lockStatus == true)
+		{
+			isLocked = true;
+			transform.Translate(Vector3.forward * transform.localScale.z);
+		}
+		else
+		{
+			isLocked = false;
+			transform.Translate(Vector3.back * transform.localScale.z);
+		}
+	}
+}
